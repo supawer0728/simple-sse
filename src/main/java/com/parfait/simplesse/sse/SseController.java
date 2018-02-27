b166b0a90e7466c379971c22c52a36229508d127
supawer0728 <supawer0728@gmail.com>
2018-02-27 23:14:16 +0900

    단순화

--- src/main/java/com/parfait/simplesse/sse/SseController.java
+++ src/main/java/com/parfait/simplesse/sse/SseController.java
@@ -2,7 +2,7 @@ package com.parfait.simplesse.sse;
 
 import com.parfait.simplesse.sse.model.MemoryInfo;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,42 +11,51 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Slf4j
 @RestController
 public class SseController {
 
-    private final CopyOnWriteArrayList<SseEmitter> emitters = new CopyOnWriteArrayList<>();
+    private final Map<SseEmitter, AtomicInteger> emitterCount = new ConcurrentHashMap<>();
 
     @CrossOrigin("*")
     @GetMapping("/memory")
     public SseEmitter memory() {
 
         SseEmitter emitter = new SseEmitter();
-        this.emitters.add(emitter);
+        this.emitterCount.put(emitter, new AtomicInteger(0));
 
-        emitter.onCompletion(() -> this.emitters.remove(emitter));
-        emitter.onTimeout(() -> this.emitters.remove(emitter));
+        emitter.onCompletion(() -> this.emitterCount.remove(emitter));
+        emitter.onTimeout(() -> this.emitterCount.remove(emitter));
         log.info("timeout : {}", emitter.getTimeout());
 
         return emitter;
     }
 
-    @EventListener
-    public void onMemoryInfo(MemoryInfo memoryInfo) {
+    @Scheduled(fixedRate = 3000L)
+    public void onMemoryInfo() {
 
         List<SseEmitter> deadEmitters = new ArrayList<>();
-        this.emitters.forEach(emitter -> {
+        this.emitterCount.forEach((emitter, count) -> {
+
+            int increasedCount = count.incrementAndGet();
             try {
-                emitter.send(memoryInfo);
-                emitter.complete();
+                emitter.send(MemoryInfo.create());
+                // spring.mvc.async.request-timeout 시간 이내에 emitter.complete()를 호출해야 Exception이 발생하지 않음
+                // 시간이 지나서 발생하는 Exception은 Dispatcher Servlet이 처리하며 여기서 딱히 잡지는 않음
+                if (increasedCount > 2) {
+                    emitter.complete();
+                }
             } catch (IOException e) {
+                log.error(e.getMessage(), e);
                 deadEmitters.add(emitter);
             }
 
         });
 
-        this.emitters.removeAll(deadEmitters);
+        deadEmitters.forEach(emitterCount::remove);
     }
 }
